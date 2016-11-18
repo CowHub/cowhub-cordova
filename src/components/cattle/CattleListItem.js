613ba8db319586938860af588fa2cb9b29af77d0
Tom Szyszko <ts3413@ic.ac.uk>
2016-11-18 20:46:05 +0000

    Enabled drag to refresh

--- src/components/cattle/CattleListItem.js
+++ src/components/cattle/CattleListItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import {
     Page,
     Button,
@@ -13,22 +14,72 @@ import {
 
 } from 'react-onsenui';
 
+const mapStateToProps = (state, ownProps) => {
+  return {
+    ...state.cattle.cattle[ownProps.id],
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+  }
+};
+
 
 class CattleListItem extends React.Component {
+
+  static propTypes = {
+    cattle: React.PropTypes.shape({
+      breed: React.PropTypes.string,
+      check_digit: React.PropTypes.number.isRequired,
+      country_code: React.PropTypes.string.isRequired,
+      dob: React.PropTypes.string,
+      gender: React.PropTypes.string,
+      herdmark: React.PropTypes.string.isRequired,
+      id: React.PropTypes.number.isRequired,
+      individual_number: React.PropTypes.number.isRequired,
+      name: React.PropTypes.string,
+    }).isRequired
+  };
+  static defaultProps = {
+    cattle: {
+      id: -1,
+      check_digit: -1,
+      country_code: '',
+      herdmark: '',
+      individual_number: -1,
+    }
+  };
+
   render() {
+    const {
+        country_code,
+        herdmark,
+        check_digit,
+        id,
+        individual_number,
+        name,
+    } = this.props.cattle;
 
     return (
-        <ListItem modifier="chevron" style={styles.listItemContainer}>
+        <ListItem style={styles.listItemContainer} modifier="chevron" >
           <Row>
             <Col width="95px">
-              <img src="http://placehold.it/350x150" style={styles.thumbnail} ></img>
+              <img src="img/icon.png" style={styles.thumbnail} ></img>
             </Col>
             <Col>
               <div style={styles.name}>
-                Cow 1
+                Herdmark: {herdmark}
               </div>
-              <div class={styles.desc}>
-                Some other information
+              <div style={styles.desc}>
+                Country Code: {country_code}
+              </div>
+                <div style={styles.desc}>
+                Check Digit :{check_digit}
+                </div>
+                  <div style={styles.desc}>
+                Individual Number : {individual_number}
+                  
               </div>
             </Col>
             <Col width="40px"></Col>
@@ -58,3 +109,5 @@ const styles ={
           fontSize: '13px'
         }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(CattleListItem);
--- src/containers/MyHerdPage.js
+++ src/containers/MyHerdPage.js
@@ -10,32 +10,54 @@ import {
     ToolbarButton,
     Row,
     Col,
-    Fab
+    Fab,
+    PullHook,
+    ProgressCircular
 
 } from 'react-onsenui';
 
 import TopBar from '../components/TopBar';
 import CreateCattlePage from './CreateCattlePage'
 import IdentifyCattlePage from './IdentifyCattlePage'
+import CattleListItem from '../components/cattle/CattleListItem.js'
+
+import {
+    fetchCattle
+} from '../actions/cattle';
 
 const mapStateToProps = (state) => {
   return {
-    ...state
+    cattleSize: state.cattle.cattle.length,
+    isFetching: state.cattle.fetching,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {}
+  return {
+    fetchCattle: () => { dispatch(fetchCattle()); },
+  }
 };
 
 
 class MyHerdPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      state: 'initial'
+    };
+  }
 
   static propTypes = {
     token: React.PropTypes.string,
     email: React.PropTypes.string,
     password: React.PropTypes.string
   };
+
+  componentWillMount() {
+    this.props.fetchCattle();
+  }
 
   handleNewClick() {
     this.props.navigator.pushPage({comp: CreateCattlePage,key:'CREATE_CATTLE_PAGE'})
@@ -45,30 +67,93 @@ class MyHerdPage extends React.Component {
     this.props.navigator.pushPage({comp: IdentifyCattlePage,key:'IDENTIFY_CATTLE_PAGE'})
   }
 
+  renderCattle() {
+    let cattle = [];
+    if (this.props.cattleSize > 0) {
+      let counter = 0;
+      while (counter < this.props.cattleSize) {
+        cattle.push(<CattleListItem  key={ counter } id={ counter } />);
+        counter += 1;
+      }
+    }
+    return cattle;
+  }
 
-  render()  {
+  renderLoading() {
     return (
         <Page renderToolbar={() => <TopBar title='My Herd' navigator={this.props.navigator} />}>
-          <div style={styles.page_content}>
-            <Row style={{'height': '20%'}}>
-              <div className='center'>
-                <img src='img/logo.jpg' style={styles.logo_img}/>
-              </div>
-            </Row>
-            <Row style={{'height': '80%'}}>
-            </Row>
-          </div>
-          <Fab
-              onClick={() => this.handleNewClick()}
-              position='bottom right'>
-            <Icon icon='md-file-plus' />
-          </Fab>
-          <Fab
-              onClick={() =>this.handleCameraClick()}
-              position='bottom left'>
-            <Icon icon='md-camera' />
-          </Fab>
-        </Page>
+          <h2>Loading</h2>
+
+    </Page>)
+  }
+
+
+  handleChange(e) {
+    this.setState({
+      state: e.state
+    });
+  }
+
+  handleLoad(done) {
+    this.props.fetchCattle();
+    setTimeout(done, 1000);
+  }
+
+  get content() {
+    switch (this.state.state) {
+      case 'initial':
+        return 'Pull to refresh';
+      case 'preaction':
+        return 'Release';
+      case 'action':
+        return 'Loading...';
+    }
+  }
+
+  renderLoadingSpiral() {
+    return (this.props.isFetching? <ProgressCircular indeterminate />:null);
+  }
+
+  renderPage()  {
+
+
+    return (
+
+    <Page renderToolbar={() => <TopBar title='My Herd' navigator={this.props.navigator} />}>
+      <PullHook
+          onChange={this.handleChange.bind(this)}
+          onLoad={this.handleLoad.bind(this)}
+          height={100}
+          thresholdHeight={150}>
+        {this.content}
+      </PullHook>
+
+      <div style={styles.page_content}>
+        {this.renderCattle()}
+      </div>
+
+      <Fab
+          onClick={() => this.handleNewClick()}
+          position='bottom right'>
+        <Icon icon='md-file-plus' />
+      </Fab>
+      <Fab
+          onClick={() =>this.handleCameraClick()}
+          position='bottom left'>
+        <Icon icon='md-camera' />
+      </Fab>
+
+    </Page>
+    )
+  }
+
+
+  render()  {
+
+    return (
+
+        this.renderPage()
+
     )
   }
 
