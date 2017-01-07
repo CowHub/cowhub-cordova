ae7b10c1cd5acfdcde26b2af2345f3551687b560
KarimNahas <kn1114@ic.ac.uk>
2017-01-07 00:03:22 +0000

    Basic refactor of login page

--- src/containers/LoginPage.js
+++ src/containers/LoginPage.js
@@ -1,131 +1,146 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import { notification } from 'onsenui';
-import {
-    Page,
-    Button,
-    Toolbar,
-    Icon,
-    Input,
-    ToolbarButton,
-    Row,
-    Col
-} from 'react-onsenui';
+import { Page, Button, Toolbar, Icon, Input, Row } from 'react-onsenui';
 
-import CattleEditTopBar from '../components/cattle/CattleEditTopBar'
-import {
-    loginUser,
-    loginErrorSeen,
-    enterEmail,
-    enterPassword,
-    submitPressed,
-    loadMyHerdPage,
-    initialTokenCheck
-} from '../actions/index';
+import { loginUser, loginErrorSeen, enterEmail, enterPassword, submitPressed,
+  initialTokenCheck } from '../actions/index';
+
 const mapStateToProps = (state) => {
   return {
-    ...state
+    token: state.authentication.token,
+    email: state.login.email,
+    password: state.login.password,
+    isFetching: state.authentication.fetching
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    handleEmail: (email) => {
-      dispatch(enterEmail(email));
-    },
-    handlePassword: (password) => {
-      dispatch(enterPassword(password))
-    },
+    handleEmail: (email) => { dispatch(enterEmail(email)) },
+    handlePassword: (password) => { dispatch(enterPassword(password)) },
     handleLogin: (params) => {
       dispatch(loginUser(params));
       dispatch(submitPressed());
     },
-    handleMyHerdPageLoad: (params) => {
-      dispatch(loadMyHerdPage());
-    },
-    handleErrorSeen:  (params)  =>  {
-      dispatch(loginErrorSeen());
-    },
-    handleFetchToken: () => {
-      dispatch(initialTokenCheck());
-    }
+    handleErrorSeen: (params) => { dispatch(loginErrorSeen()) },
+    handleFetchToken: () => { dispatch(initialTokenCheck()) }
   };
 };
-
 
 class LoginPage extends React.Component {
 
   static propTypes = {
     token: React.PropTypes.string,
     email: React.PropTypes.string,
-    password: React.PropTypes.string
+    password: React.PropTypes.string,
+    isFetching: React.PropTypes.bool
   };
 
+  componentWillMount() {
+    this.handleError(this.props);
+  }
+
   componentDidMount() {
-    this.handleAuthenticated(this.props);
+    if (!this.props.isFetching)
+      this.props.handleFetchToken();
   }
 
-  handleAuthenticated(props) {
-    if (!props.authentication.fetching) props.handleFetchToken();
+  componentWillReceiveProps(props) {
+    this.handleError(props);
   }
 
-  submit() {
-    this.props.handleLogin({
-      email: this.props.login.email,
-      password: this.props.login.password
-    })
+  handleError(props) {
+    return props.error
+      ? notification.alert({
+        title: 'Error',
+        message: props.error.responseJSON ? props.error.responseJSON.errors[0] : props.error.responseText,
+        callback: props.handleErrorSeen
+      })
+      : null;
   }
 
+  renderToolbar() {
+    return (
+      <Toolbar>
+        <div className='center'>Login</div>
+      </Toolbar>
+    );
+  }
 
-  error() {
-    return this.props.authentication.error?
-        notification.alert({
-          message: 'Error: '+ this.props.authentication.error.responseJSON?
-              this.props.authentication.error.responseJSON.errors[0] : null,
-          callback: this.props.handleErrorSeen
-        })
-        :
-        null;
+  renderLogo() {
+    return (
+      <Row style={{'height': '20%'}}>
+        <div className='center'>
+          <img src='img/logo.jpg' style={ styles.logo_img }/>
+        </div>
+      </Row>
+    );
+  }
+
+  renderForm() {
+    return (
+      <Row style={{'height': '80%'}}>
+        <Row style={{'height': '30%'}}/>
+        <Row style={{'height': '5%'}}>
+          { this.renderEmailInput() }
+        </Row>
+        <Row style={{'height': '5%'}}/>
+        <Row style={{'height': '5%'}}>
+          { this.renderPasswordInput() }
+        </Row>
+        <Row style={{'height': '20%'}}/>
+        <Row style={{'height': '5%'}}>
+          { this.renderButton() }
+        </Row>
+      </Row>
+    );
+  }
+
+  renderEmailInput() {
+    return (
+      <Input ref='email' placeholder='Email' type='text'
+        value={this.props.email} modifier='underbar'
+        onChange={ (event) => this.props.handleEmail(event.target.value) }
+        style={{width:'100%'}}
+      />
+    );
+  }
+
+  renderPasswordInput() {
+    return (
+      <Input ref='password' placeholder='Password' type='password'
+        value={this.props.password} modifier='underbar'
+        onChange={ (event) => this.props.handlePassword(event.target.value) }
+        style={{width:'100%'}}
+      />
+    );
+  }
+
+  renderButton() {
+    return (
+      <Button id='signIn' className='signIn' modifier='large'
+        onClick={ () => this.props.handleLogin({
+          email: this.props.email,
+          password: this.props.password
+        })}
+      >
+        Sign In
+      </Button>
+    );
   }
 
   render()  {
     return (
-        <Page renderToolbar={() => <CattleEditTopBar title='Login'  />}>
-          <div style={styles.page_content}>
-            <Row style={{'height': '20%'}}>
-              <div className='center'>
-                <img src='img/logo.jpg' style={styles.logo_img}/>
-              </div>
-            </Row>
-            <Row style={{'height': '80%'}}>
-              <Row style={{'height': '30%'}}>
-                {this.error()}
-              </Row>
-              <Row style={{'height': '5%'}}>
-                <Input ref="email" placeholder="Email" type="text" modifier="underbar" value={this.props.login.email}
-                       onChange={(event) => {
-                this.props.handleEmail(event.target.value)
-                }} style={{width:'100%'}}/>
-              </Row>
-              <Row style={{'height': '5%'}}></Row>
-              <Row style={{'height': '5%'}}>
-                <Input ref="password" placeholder="Password" type="password" value={this.props.login.password}
-                       modifier="underbar" onChange={(event) =>
-                this.props.handlePassword(event.target.value)} style={{width:'100%'}}/>
-              </Row>
-              <Row style={{'height': '20%'}}></Row>
-              <Row style={{'height': '5%'}}>
-                <Button id='signIn' onClick={
-                () => this.submit() } className='signIn' modifier="large">Sign In</Button>
-              </Row>
-            </Row>
-          </div>
-        </Page>
-    )
+      <Page renderToolbar={ () => this.renderToolbar() }>
+        <div style={styles.page_content}>
+          { this.renderLogo() }
+          { this.renderForm() }
+        </div>
+      </Page>
+    );
   }
-
 }
-
 
 const styles = {
   logo_img: {
@@ -140,6 +155,5 @@ const styles = {
     height: '90%'
   }
 };
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
