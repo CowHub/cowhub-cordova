0214b7f65d435e159ead348a818dfea8882a4116
KarimNahas <kn1114@ic.ac.uk>
2017-01-08 12:43:57 +0000

    Updated prop types

--- src/utilities/CustomPropTypes.js
+++ src/utilities/CustomPropTypes.js
@@ -8,12 +8,24 @@ export default class CustomPropTypes {
     country_code: React.PropTypes.string,
     dob: React.PropTypes.string,
     gender: React.PropTypes.string,
+    genetic_dam: React.PropTypes.string,
     herdmark: React.PropTypes.string,
     id: React.PropTypes.number,
+    image_ids: React.PropTypes.arrayOf(React.PropTypes.number),
     images: React.PropTypes.arrayOf(React.PropTypes.string),
     individual_number: React.PropTypes.number,
-    name: React.PropTypes.string
+    location: CustomPropTypes.location,
+    name: React.PropTypes.string,
+    sir_dam: React.PropTypes.string,
+    surrogate_dam: React.PropTypes.string,
   };
 
   static cattle = React.PropTypes.shape(CustomPropTypes._cattleObject);
+
+  static _location = {
+    latitude: React.PropTypes.number,
+    longitude: React.PropTypes.number
+  };
+
+  static location = React.PropTypes.shape(CustomPropTypes._location);
 }
